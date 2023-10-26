bca965e571dc1c1114ffe5eb5f1d18f475f6bfc3
frankpagan <frank@cocreate.app>
2023-10-26 01:32:44 -0600

    fix: update host localhost:3000

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "organization_id": "652c8d62679eca03e0b116a7",
-    "host": "vocationvisions.com",
+    "host": "ws://localhost:3000",
     "apikey": "a68534d5-6641-419a-b974-60f0757ae405",
     "directories": [
         {
