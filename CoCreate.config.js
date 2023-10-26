02eb6b5c28eb012041108706be7f9b5e2671a476
frankpagan <frank@cocreate.app>
2023-10-25 19:31:54 -0600

    fix: added config details

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,10 +1,10 @@
 module.exports = {
+    "organization_id": "652c8d62679eca03e0b116a7",
+    "host": "vocationvisions.com",
+    "apikey": "a68534d5-6641-419a-b974-60f0757ae405",
     "directories": [
         {
             "entry": "./src",
-            "exclude": [
-                "demos"
-            ],
             "array": "files",
             "object": {
                 "name": "{{name}}",
@@ -17,22 +17,6 @@ module.exports = {
                 "pathname": "{{pathname}}",
                 "content-type": "{{content-type}}",
                 "public": "true"
-            }
-        }
-    ],
-    "sources": [
-        {
-            "array": "demos",
-            "object": {
-                "_id": "619d495ca8b6b4001a9e6127",
-                "collaboration-demo": "{{./src/demos/collaboration.html}}"
-            }
-        },
-        {
-            "array": "demos",
-            "object": {
-                "_id": "639b325f6f44f0f162f0bcbf",
-                "demo-code": "{{./src/demos/demo.html}}"
             }
         }
     ]
