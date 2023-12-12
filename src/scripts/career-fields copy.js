a0ddbba3b74df775c2f8ea0d9ba8edd8aeaf7ce4
frankpagan <frank@cocreate.app>
2023-12-12 00:53:00 -0600

    fix: feild name backup

---
+++ src/scripts/career-fields copy.js
@@ -0,0 +1,197 @@
+`Activities
+Advancement Potential
+Advancement, Time it takes
+Afternoons
+Alternative Qualifications
+Apprentice
+Areas of growth
+Areas of Knowledge
+Assets
+Attributes Personal
+Autonomously
+Background
+Business Practices
+Career Longevity
+Career Path Variability
+Career Reputation
+Ceremonies
+Certification
+Challenges
+Characteristics
+Clients
+Company links / subsidiaries / affiliates
+Conflict
+Constraints
+Convictions
+Creativity
+Credentials
+Cultural Diversity
+Culture
+Customers
+Dangerous
+Days
+Decisions made / Decision making
+Demand Trends
+Departments
+Descriptions
+Developments on Horizon
+Direction
+Discount
+Dividends
+Divorce Rate
+Duties
+Educational Requirements
+Effects
+Emotional Quotient(EQ)
+Entering, too many or too few
+Entertainment Business
+Entertainment Business Late Night
+Entry Level Opportunities
+Entry Point
+Environmental Impact
+Ethical Considerations
+Experience
+Family Life
+Field
+Flexibility
+Front - Line
+Frustrations
+Functions
+Geographic Mobility
+GPA - Grade Point Average
+Graduate Degree
+Groups
+Hazard
+Health Risks
+Hierarchy
+Hiring
+Hours
+Hours, Average per day
+Important people in Industry
+Incentives
+Individual
+Industry, Change
+Industry, Growth
+Industry, guidebooks
+Industry, Issues
+Industry, Reputation
+Information
+Innovation
+Insurable
+Insurance Rated
+Interact
+Interests
+Intern
+Internal
+IQ
+Isolated
+Interesting
+Job Mortality
+Key Skills
+Layoffs
+Learning
+Levels
+Liability
+Licenses
+Life Expectancy
+Lifestyle
+Major in College
+Management
+Markets
+MBA
+Mental
+Military
+Multitasking
+Morale
+Net Worth
+Nights
+Obstacles
+Occupation
+Office
+Opportunities
+Organizations
+Organized
+Outlook
+Overtime
+Owner
+Ownership Potential
+Partnership
+Part - Time
+Passport
+Pay / Salary ceiling
+Percent men
+Percent women
+Perform
+Personality Traits
+Philosophy
+Phone
+Position
+Preconditions
+Problems
+Professional Development
+Professional Journals
+Professional Networking
+Professional Organizations
+Preparation, educational
+Prestige
+Qualifications
+Regulations
+Reimbursement
+Remote Working Possibilities
+Repetitious
+Reputation
+Residence
+Resume
+Requirements
+Responsibilities
+Rituals
+Sacrifices
+Salary Range
+Satisfaction
+Satisfaction Industry
+Schedule
+Seasonal
+Seasons, Toughest
+Security Clearance
+Security Job
+Shadow
+Skills Required
+Social
+Social obligations
+Status
+Stress
+Studies
+Subordinate
+Success
+Suicide Rate
+Supervise
+Take Work Home
+Teams
+Technological Integration
+Technology
+Time Use
+Title
+Top - Down
+Traditions
+Transferable Skills
+Travel
+Travel, frequent
+Training
+Training Formal
+Trends
+Trends Emerging
+Tuition
+Turnover
+Turnover, reason leave field
+Unique
+Vacancies
+Vacation
+Values
+Visa
+Volunteer
+Warning
+Wavers
+Weekends
+Work Environment
+Work - Life Balance
+`
