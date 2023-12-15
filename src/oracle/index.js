97b72b22bbe73c0c6d7a3eec18aa04372d40ff9a
frankpagan <frank@cocreate.app>
2023-12-15 07:25:57 -0600

    feat: custom function to handle career responses

---
+++ src/oracle/index.js
@@ -0,0 +1,139 @@
+document.addEventListener('actionsLoaded', function () {
+    CoCreate.actions.init({
+        name: "manageCareers",
+        callback: async (action) => {
+            let value = action.form[0].getValue()
+            const element = document.getElementById('mergedCareers');
+
+            let preValue = element.getValue()
+            let newValue = [...value, ...preValue]
+            element.setValue(newValue)
+            document.dispatchEvent(new CustomEvent('manageCareers', {
+                detail: {}
+            }));
+
+            let careers = await CoCreate.socket.send({
+                method: "object.read",
+                array: "careers",
+                $filter: {
+                    query: [
+                        { key: "name", value, operator: '$in' }
+                    ]
+                }
+            });
+
+            for (let careerName of value) {
+                const careerExists = careers.object.some(existingCareer => existingCareer.name === careerName);
+
+                if (!careerExists) {
+                    await addCareer(careerName)
+                }
+            }
+
+        }
+    });
+}, { once: true });
+
+
+const careerFields = [`
+    name
+    salary
+    stress
+    pros
+    cons
+    facts
+    activities
+    advancement
+    alternativeQualifications
+    apprentice
+    areasGrowth
+    areasKnowledge
+    assets
+    attributesPersonal
+    background
+    careerLongevity
+    careerPathVariability
+    careerReputation
+    challenges
+    characteristics
+    constraints
+    credentials
+    decisionMaking
+    descriptions
+    effects
+    entryPoint
+    familyLife
+    frustrations
+    importantIndustryPeople
+    interests
+    keySkills
+    professionalJournals
+    professionalOrganizations
+    sacrifices
+    satisfaction
+    supervise
+    takeWorkHome
+    technology
+    vacation
+    `
+]
+
+
+async function addCareer(name) {
+    try {
+        let careersDetails = {}
+        for (let i = 0; i < careerFields.length; i++) {
+            const messages = []
+            messages.push({
+                role: 'system',
+                content: `Need details about the career name provided. Your response needs to be a valid Json object containing the following keys with no additional context required. please be sure it is valid JSON as it will be parsed:
+                    ${careerFields[i]}
+                    `
+            })
+            messages.push({ role: 'user', content: name })
+
+            let data = await CoCreate.socket.send({
+                method: 'openai.chat',
+                chat: {
+                    model: 'gpt-3.5-turbo',
+                    messages,
+                    max_tokens: 3300,
+                    temperature: 0.6,
+                    n: 1,
+                    stop: '###STOP###',
+                }
+            })
+
+            if (data.chat.choices && data.chat.choices[0].message.content) {
+                let content = data.chat.choices[0].message.content;
+                content = content.replace(/```json\n|\n```/g, '');
+                content = content.replace(/```javascript\n|\n```/g, '');
+                try {
+                    content = JSON.parse(content)
+                    careersDetails = { ...careersDetails, ...content }
+                } catch (error) {
+                    console.error(`Error processing ${name}: ${error}`);
+                }
+            } else {
+                console.error(`Error processing ${name}: ${error}`);
+            }
+        }
+
+        let request = {
+            method: "object.create",
+            array: "careers",
+            object: {
+                ...careersDetails
+            }
+        }
+
+        data = await CoCreate.crud.send(request)
+        if (!data || !data.object || !data.object[0]) {
+            console.error(`Error processing ${name}: ${error}`);
+        }
+        else
+            console.log('created career: ', data.object[0])
+    } catch (error) {
+        console.error(`Error processing ${name}: ${error}`);
+    }
+}
