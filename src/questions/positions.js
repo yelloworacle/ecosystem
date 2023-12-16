320d99c0705678151ba63c957b615efae50bd656
frankpagan <frank@cocreate.app>
2023-12-15 21:36:14 -0600

    feat: position.js handles dynamic position updates

---
+++ src/questions/positions.js
@@ -0,0 +1,46 @@
+CoCreate.actions.init({
+    name: "updatePositions",
+    callback: async (action) => {
+
+        let name = action.form.querySelector('#name');
+        name = name.getValue()
+
+        let position = action.form.querySelector('#position');
+        position = position.getValue()
+
+        let data = await CoCreate.socket.send({
+            method: "object.read",
+            array: "questions",
+            $filter: {
+                sort: [
+                    { key: 'position', direction: 'asc' }
+                ]
+            }
+        });
+
+        let currentPosition = 0
+        let questions = data.object
+        for (let i = 0; i < questions.length; i++) {
+            currentPosition += 1
+            if (questions[i].position === currentPosition && questions[i].position !== position)
+                continue
+            if (questions[i].name === name)
+                continue
+            if (questions[i].position === position && currentPosition === position)
+                currentPosition += 1
+
+            await CoCreate.crud.send({
+                method: "object.update",
+                array: "questions",
+                object: {
+                    _id: questions[i]._id,
+                    position: currentPosition
+                }
+            });
+        }
+
+        document.dispatchEvent(new CustomEvent('updatePositions', {
+            detail: {}
+        }));
+    }
+});
