8d15e980689eba278ed83b0c0dbc08b2f37bd7ce
frankpagan <frank@cocreate.app>
2023-12-21 13:40:46 -0600

    fix test and production mode

--- src/pricing/pricing.js
+++ src/pricing/pricing.js
@@ -1,6 +1,19 @@
-var stripe = Stripe(
-    "pk_test_51OK9b5DSpIU4j2Joz6AeDvWPQEpoWhRweNS6Hevwvza5IcHwTpgTGTNU5GGMZUiQYzeSBifqV8AWaT3OtjUAVwXt00c3hmBILe"
-); // Replace with your publishable key
+const environment = "production" // production, test
+let productionPk = "pk_live_51OK9b5DSpIU4j2Jo6VbxSw3F8LHhru5wn2FZyE7pqWtRqHoD1hBrtDkxMySPpJNEyGTEoPE0Nfs7bGFqkB5UX17300yYKufL6v"
+let testPk = "pk_test_51OK9b5DSpIU4j2Joz6AeDvWPQEpoWhRweNS6Hevwvza5IcHwTpgTGTNU5GGMZUiQYzeSBifqV8AWaT3OtjUAVwXt00c3hmBILe"
+
+
+let pk
+if (environment === 'production')
+    pk = productionPk
+else
+    pk = testPk
+
+const stripe = Stripe(pk);
+
+// "pk_test_51OK9b5DSpIU4j2Joz6AeDvWPQEpoWhRweNS6Hevwvza5IcHwTpgTGTNU5GGMZUiQYzeSBifqV8AWaT3OtjUAVwXt00c3hmBILe"
+// "pk_live_51OK9b5DSpIU4j2Jo6VbxSw3F8LHhru5wn2FZyE7pqWtRqHoD1hBrtDkxMySPpJNEyGTEoPE0Nfs7bGFqkB5UX17300yYKufL6v"
+
 var elements = stripe.elements();
 
 var style = {
@@ -43,29 +56,36 @@ form.addEventListener("submit", function (event) {
                     "price_1OP7WaDSpIU4j2JoOhYOpB4U";
                 additionalData.subscription_id =
                     "6571fe530c48ef6970900a82";
-                let subscription = await CoCreate.socket.send({
+                let data = await CoCreate.socket.send({
                     method: "stripe.subscriptions.create",
                     broadcast: false,
                     stripe: additionalData,
+                    environment
                 });
 
-                console.log("subscription", subscription);
-                // let userSubscription = await CoCreate.crud.send({
-                //     method: "object.update",
-                //     broadcast: false,
-                //     array: "users",
-                //     object: {
-                //         _id: subscription.user_id,
-                //         subscription: "6571fe530c48ef6970900a82",
-                //     },
-                // });
+                if (data.stripe.subscription.id) {
+                    CoCreate.crud.send({
+                        method: "object.update",
+                        broadcast: false,
+                        array: "users",
+                        object: {
+                            _id: data.user_id,
+                            subscription: "6571fe530c48ef6970900a82",
+                            subscriptionId: data.stripe.subscription.id
+                        },
+                    });
 
-                submitButton.innerHTML = "Payment Successful";
-                window.localStorage.setItem('subscription', '6571fe530c48ef6970900a82')
-                setTimeout(function () {
-                    // window.location = "/";
-                    window.location.href = "/"
-                }, 3000);
+                    submitButton.innerHTML = "Payment Successful";
+                    window.localStorage.setItem('subscription', '6571fe530c48ef6970900a82')
+
+                    setTimeout(function () {
+                        // window.location = "/";
+                        window.location.href = "/"
+                    }, 3000);
+                } else {
+                    submitButton.innerHTML = "Payment Failed";
+                }
+
             }
         });
 });
