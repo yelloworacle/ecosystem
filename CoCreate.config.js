df5142868635b4a472385fd118f3e0bd60c76d46
frankpagan <frank@cocreate.app>
2024-01-09 17:22:10 +0000

    feat: define host.$branch to handle environments

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     "organization_id": "652c8d62679eca03e0b116a7",
-    "host": "wss://yelloworacle.com",
+    "host": {
+        "$branch": {
+            "main": "wss://yelloworacle.com",
+            "dev": "wss://dev.yelloworacle.com"
+        }
+    },
     "apikey": "c685ywd5-9031-224a-b974-60f07jety103",
     "directories": [
         {
