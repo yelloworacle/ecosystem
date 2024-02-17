a90778721cb821977b1f4ac056e650a6104130ec
frankpagan <frank@cocreate.app>
2024-02-17 15:38:20 -0500

    fix: $ip value

--- src/account/ambassador/index.js
+++
@@ -1,8 +0,0 @@
-let input = document.querySelector('[value="$ip"]')
-if (input) {
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            input.value = data.ip;
-        })
-}
