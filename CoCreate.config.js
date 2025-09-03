5f5bd1b01bcf5bae524c855c5e6fcf2bcfa3e8f2
frankpagan <frank@cocreate.app>
2025-09-03 02:55:03 -0600

    Fix asset paths and update href attributes across multiple components
    - Corrected asset paths in ai-chat.html, ai.html, datatable-toolbar.html, footer.html, navbar.html, org-selector.html, sidenav.html to include a missing slash.
    - Updated href attributes in various components to ensure proper routing.
    - Ensured consistency in asset references for SVG icons and images.

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,32 +1,28 @@
 module.exports = {
-    "organization_id": "652c8d62679eca03e0b116a7",
-    "host": {
-        "$branch": {
-            "main": "wss://yelloworacle.com",
-            "dev": "wss://dev.yelloworacle.com",
-            "test": "wss://test.yelloworacle.com"
-        }
-    },
-    "apikey": "c685ywd5-9031-224a-b974-60f07jety103",
-    "directories": [
-        {
-            "entry": "./src",
-            "exclude": [
-                ".txt"
-            ],
-            "array": "files",
-            "object": {
-                "name": "{{name}}",
-                "src": "{{source}}",
-                "host": [
-                    "*"
-                ],
-                "directory": "/",
-                "path": "{{path}}",
-                "pathname": "{{pathname}}",
-                "content-type": "{{content-type}}",
-                "public": "true"
-            }
-        }
-    ]
+	organization_id: "652c8d62679eca03e0b116a7",
+	host: {
+		$branch: {
+			main: "wss://yelloworacle.com",
+			dev: "wss://dev.yelloworacle.com",
+			test: "wss://test.yelloworacle.com"
+		}
+	},
+	apikey: "c685ywd5-9031-224a-b974-60f07jety103",
+	directories: [
+		{
+			entry: "./src",
+			exclude: [".txt", "assets", "admin"],
+			array: "files",
+			object: {
+				name: "{{name}}",
+				src: "{{source}}",
+				host: ["*"],
+				directory: "/",
+				path: "{{path}}",
+				pathname: "{{pathname}}",
+				"content-type": "{{content-type}}",
+				public: "true"
+			}
+		}
+	]
 };
