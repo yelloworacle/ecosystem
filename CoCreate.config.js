bb32caab726bc745947ea1d7299c6ddf014231da
frankpagan <frank@cocreate.app>
2025-09-05 22:25:52 -0600

    fix: update asset paths and refine form structure in AI component

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	directories: [
 		{
 			entry: "./src",
-			exclude: [".txt", "assets", "admin"],
+			exclude: [".txt"],
 			array: "files",
 			object: {
 				name: "{{name}}",
